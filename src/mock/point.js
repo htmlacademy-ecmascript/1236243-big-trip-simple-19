7bf026b3c62db2f8a9f5219e32f936da266c7e13
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2022-12-26 21:31:06 +0300

    Сделал моки v 1.5

--- src/mock/point.js
+++ src/mock/point.js
@@ -2,18 +2,6 @@ import {TYPES, CITIES, TRIPS, TITLE_OFFERS} from '../const.js'
 import {getRandomArrayElement, getRandomNumber} from '../utils.js'
 import {mockOffers} from '../mock/offer.js'
 
-const mockPoint = [
-    {
-        base_price: getRandomNumber(500, 1500),
-        date_from: "2019-07-10T22:55:56.845Z",
-        date_to: "2019-07-11T11:22:13.375Z",
-        destination: getRandomArrayElement(CITIES),
-        id: 1,
-        type: getRandomArrayElement(TYPES),
-        offers: getRandomArrayElement(TITLE_OFFERS).offer
-        }
-    ]
-
 
 const getMockPoints = () => {
     const  points= []

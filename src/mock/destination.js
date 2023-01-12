ef32cb26ef07c3bc9387c9ff065a590746db190d
Игорь Яковлев <iakovlev.igor.dev@gmail.com>
2023-01-12 14:57:25 +0300

    Рефакторинг и обработчики событий

--- src/mock/destination.js
+++ src/mock/destination.js
@@ -4,20 +4,7 @@ import { getRandomArrayElement } from '../utils.js';
 
 const getDestination = function () {
   const destination = [];
-  // for (let i = 0; i < CITIES.length; i++) {
-  //   const mockDestination = {
-  //     id: i + 1,
-  //     description: getRandomArrayElement(DESCRIPTION),
-  //     name: getRandomArrayElement(CITIES),
-  //     pictures:
-  //       {
-  //         src: 'http://picsum.photos/300/200?r=Math.random()',
-  //         description: getRandomArrayElement(DESCRIPTION)
-  //       }
-  //   };
 
-  //   destination.push(mockDestination);
-  // }
   CITIES.forEach((el, i) => {
     const mockDestination = {
       id: i + 1,
--- src/presenter/trip_presenter.js
+++ src/presenter/trip_presenter.js
@@ -2,31 +2,70 @@ import { render } from '../render.js';
 import FiltersSortView from '../view/filters_sort.js';
 import TripListView from '../view/trip_view_list.js';
 import RoutePoint from '../view/route_point.js';
-import FormCreationView from '../view/form_creation.js';
+// import FormCreationView from '../view/form_creation.js';
 import FormEditView from '../view/form_edit.js';
 
 
 export default class TripPresenter {
-  tripListComponent = new TripListView();
+  #tripListComponent = new TripListView();
+
+  #tripContainer = null;
+  #pointsModel = null;
+
+  #boardPoints = [];
+  #boardOffers = [];
 
   constructor ({tripContainer, pointsModel}) {
-    this.tripContainer = tripContainer;
-    this.pointsModel = pointsModel;
+    this.#tripContainer = tripContainer;
+    this.#pointsModel = pointsModel;
 
   }
 
   init() {
-    this.boardPoints = [...this.pointsModel.getPoints()];
-    this.boardOffers = [...this.pointsModel.getOffers()];
+    this.#boardPoints = [...this.#pointsModel.points];
+    this.#boardOffers = [...this.#pointsModel.offers];
 
-    render(new FiltersSortView(), this.tripContainer);
-    render(this.tripListComponent, this.tripContainer);
-    render(new FormEditView({point: this.boardPoints[0]}, {offer: this.boardOffers[0]}), this.tripListComponent.getElement());
+    render(new FiltersSortView(), this.#tripContainer);
+    render(this.#tripListComponent, this.#tripContainer);
 
-    for (let i = 0; i < this.boardPoints.length; i++) {
-      render(new RoutePoint({point: this.boardPoints[i]}, {offer: this.boardOffers[i]}), this.tripListComponent.getElement());
+    for (let i = 0; i < this.#boardPoints.length; i++) {
+      this.#renderTrip(this.#boardPoints[i], this.#boardOffers[i]);
     }
 
-    render(new FormCreationView({point: this.boardPoints[2]}, {offer: this.boardOffers[2]}), this.tripListComponent.getElement());
+  }
+
+  #renderTrip(point, offer) {
+    const tripComponent = new RoutePoint({point, offer});
+    const tripEditComponent = new FormEditView({point, offer});
+
+    const replaceTripToEdit = () => {
+      this.#tripListComponent.element.replaceChild(tripEditComponent.element, tripComponent.element);
+    };
+
+    const replaceEditToTrip = () => {
+      this.#tripListComponent.element.replaceChild(tripComponent.element, tripEditComponent.element);
+    };
+
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceEditToTrip();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
+
+    tripComponent.element.querySelector('.event__rollup-btn').addEventListener('click', ()=> {
+      replaceTripToEdit();
+      document.addEventListener('keydown', escKeyDownHandler);
+    });
+
+    tripEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      replaceEditToTrip();
+      document.removeEventListener('keydown', escKeyDownHandler);
+    });
+
+
+    render(tripComponent, this.#tripListComponent.element);
   }
 }
